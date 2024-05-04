ca01dcf6445cc952da459b3b07987733a3c611d4
Harjas Singh <83627055+harjasae2001@users.noreply.github.com>
2024-05-04 23:54:21 +0530

    Setup Proxy and used BeautifulSoup

--- requestProxy.py
+++ requestProxy.py
@@ -1,11 +1,12 @@
-import requests
+# #!/usr/bin/env python
+# print('If you get error "ImportError: No module named \'six\'" install six:\n'+\
+#     '$ sudo pip install six');
+# print('To enable your free eval account and get CUSTOMER, YOURZONE and ' + \
+#     'YOURPASS, please contact sales@brightdata.com')
 
-
-proxies = {
-    'http': 'http://brd-customer-hl_7e1496e5-zone-residential_proxy2:by37c8ur2lkb@brd.superproxy.io:22225',
-     'https': 'http://brd-customer-hl_7e1496e5-zone-residential_proxy2:by37c8ur2lkb@brd.superproxy.io:22225',
-
-            }
-
-r = requests.get('https://api64.ipify.org?format=json', proxies=proxies)
-print(r.json())
+import urllib.request
+opener = urllib.request.build_opener(
+urllib.request.ProxyHandler(
+            {'http': 'http://brd-customer-hl_7e1496e5-zone-residential_proxy2:by37c8ur2lkb@brd.superproxy.io:22225',
+            'https': 'http://brd-customer-hl_7e1496e5-zone-residential_proxy2:by37c8ur2lkb@brd.superproxy.io:22225'}))
+print(opener.open('http://lumtest.com/myip.json').read())
